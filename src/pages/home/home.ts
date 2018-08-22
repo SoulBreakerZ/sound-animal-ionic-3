16e853f4f8dbeb68f2bc32a7d411ecffc9aa505d
Reimy Antivilo <antivilo.trabajo@gmail.com>
2018-08-22 10:20:39 -0300

    Se agregan archivos de sonidos e imagenes de animales

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
+import { Animal } from './../../interfaces/animal.interface';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ANIMALES } from './../../data/data.animales';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales:Animal[] =[];
 
+  constructor(public navCtrl: NavController) {
+    this.animales = ANIMALES.splice(0);
+  }
+
+  reproducir (animal:Animal){
+    console.log(animal.audio);
+    let audio = new Audio();
+    audio.src = animal.audio;
+
+    audio.load();
+    audio.play();
+
+    animal.reproduciendo = true;
+
+    setTimeout(() => animal.reproduciendo = false , animal.duracion * 1000);
   }
 
 }
